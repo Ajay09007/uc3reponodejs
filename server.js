d0ab051b1bcebb0b260e63dffe2f9f5beb87fbe8
Ajay09007 <113970570+Ajay09007@users.noreply.github.com>
2024-07-19 17:35:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('batch825 jenkins demo! . Welcome To DevOps session. final approch is good welcome');});
+  res.send('batch825 jenkins demo! . Welcome To DevOps session. final approch is good welcome to project 3');});
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
